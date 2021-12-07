8f4e1091a4cf999ce353e11ee045ab991045652a
TorontosFinest <abdialicanada20@gmail.com>
2021-12-07 00:14:47 +0000

    added post routing for new form submissions

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -38,6 +40,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -45,6 +51,8 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
